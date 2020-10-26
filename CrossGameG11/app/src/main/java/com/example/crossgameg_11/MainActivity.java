0f3cb3853e25f6893d56a2d8a93a8752757a2423
Jano Bokuchava <m.prog@yahoo.com>
2020-10-26 14:31:19 +0400

    Media Player 11

--- CrossGameG11/app/src/main/java/com/example/crossgameg_11/MainActivity.java
+++ CrossGameG11/app/src/main/java/com/example/crossgameg_11/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.crossgameg_11;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.gridlayout.widget.GridLayout;
 
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
             {0,1,2},
             {3,4,5},
             {6,7,8},
+
             {0,3,6},
             {1,4,7},
             {2,5,8},
             {0,4,8},
             {2,4,6}
     };
+
 
     boolean gameIsActive = true;
 
@@ -52,6 +57,7 @@ public class MainActivity extends AppCompatActivity {
 ////        currentImage.animate().translationY(-1500).setDuration(1000);
 //    }
 
+
     public void onPlayGame(View view){
         ImageView currentImage = (ImageView) view;
 
@@ -75,6 +81,9 @@ public class MainActivity extends AppCompatActivity {
             for(int[] winPosition: winPositions){
                 if( gameState[winPosition[0]] != 2 && gameState[winPosition[0]] == gameState[winPosition[1]] && gameState[winPosition[1]] == gameState[winPosition[2]]) {
                     String message;
+                    Button restartGameBtn = (Button) findViewById(R.id.restartGameBtn);
+                    TextView gameMessage = (TextView) findViewById(R.id.gameMessage);
+
                     gameIsActive = false;
 
                     if( player == 0 ) {
@@ -84,11 +93,50 @@ public class MainActivity extends AppCompatActivity {
                         message = "Yellow";
                     }
 
-                    Toast.makeText(this, "Winner "+message, Toast.LENGTH_SHORT).show();
+
+                    restartGameBtn.setVisibility(View.VISIBLE);
+                    gameMessage.setVisibility(View.VISIBLE);
+
+                    gameMessage.setAlpha(0);
+                    restartGameBtn.setAlpha(0);
+
+                    gameMessage.setTranslationX(-2000);
+                    restartGameBtn.setTranslationX(2000);
+
+                    gameMessage.animate().translationX(0).alpha(1).setDuration(300);
+                    restartGameBtn.animate().translationX(0).alpha(1).setDuration(300);
+
+                    gameMessage.setText("Winner is " + message);
+
+//                    Toast.makeText(this, "Winner "+message, Toast.LENGTH_SHORT).show();
+                    break;
                 }
             }
 
+        }
 
+    }
+
+    public void restartGame(View view) {
+        Button restartGameBtn = (Button) findViewById(R.id.restartGameBtn);
+        TextView gameMessage = (TextView) findViewById(R.id.gameMessage);
+
+        restartGameBtn.setVisibility(View.INVISIBLE);
+        gameMessage.setVisibility(View.INVISIBLE);
+
+        gameIsActive = true;
+        player = 0;
+
+        for( int i = 0; i < gameState.length; i++ ) {
+            gameState[i] = 2;
+        }
+
+
+        GridLayout gridLayout = (GridLayout) findViewById(R.id.gridLayout);
+        int childCount = gridLayout.getChildCount();
+        for( int i = 0; i < childCount; i++ ) {
+            ImageView tempImage = (ImageView) gridLayout.getChildAt(i);
+            tempImage.setImageDrawable(null);
         }
 
     }
